cd0adf4675708898b33beb0b39f91f27f425b9c6
Amanda Rolon <abrolon87@gmail.com>
2019-12-25 02:57:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,17 @@ class Dog
     @breed = breed
     @name = name
   end
+
   def name=(name)
     @name = name
   end
+
   def name
     @name
   end
 
   def breed=(breed)
-    @breed
+    @breed = breed
   end
 
   def breed
